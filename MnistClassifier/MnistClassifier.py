16bc4c20a4429eb1b6ceceee3a9c66da0b36199a
JesseAgate <jesse.agate@gmail.com>
2021-01-05 14:52:28 -0500

    Basic MNIST digit Classifier

---
+++ MnistClassifier/MnistClassifier.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+#1. Load and prepare dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test,y_test) = mnist.load_data()
+x_train, x_test = x_train/ 255.0, x_test/255.0
+
+x_train = x_train[..., tf.newaxis].astype("float32")
+x_test = x_test[..., tf.newaxis].astype("float32")
+
+train_ds = tf.data.Dataset.from_tensor_slices(
+    (x_train, y_train)).shuffle(10000).batch(32)
+
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+#2. Build model
+def make_my_model():
+    inputs = tf.keras.Input(shape=(28,28,1))
+    x = layers.Conv2D(32, 3, activation='relu')(inputs)
+    x = layers.Flatten()(x)
+    x = layers.Dense(128, activation='relu')(x)
+    out = layers.Dense(10)(x)
+    return tf.keras.Model(inputs=inputs, outputs=out)
+
+model = make_my_model()
+tf.keras.utils.plot_model(model, "Test.png", show_shapes=True)
+
+#3. Define loss and optimizer
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+#4. Training Loop
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images, training=True)
+        loss = loss_object(labels,predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+#5. Testing
+@tf.function
+def test_step(images, labels):
+  # training=False is only needed if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  predictions = model(images, training=False)
+  t_loss = loss_object(labels, predictions)
+
+  test_loss(t_loss)
+  test_accuracy(labels, predictions)
+
+
+#Training Loop
+for epoch in range(5):
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  test_loss.reset_states()
+  test_accuracy.reset_states()
+
+  for images, labels in train_ds:
+    train_step(images, labels)
+
+  for test_images, test_labels in test_ds:
+    test_step(test_images, test_labels)
+
+  print(
+    f'Epoch {epoch + 1}, '
+    f'Loss: {train_loss.result()}, '
+    f'Accuracy: {train_accuracy.result() * 100}, '
+    f'Test Loss: {test_loss.result()}, '
+    f'Test Accuracy: {test_accuracy.result() * 100}'
+  )
+
+
+
+
